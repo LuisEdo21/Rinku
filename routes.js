58804ced41c15add841fa2d1e1d7cafe56a1b7e2
Luis Eduardo Villela Zavala <luisedo21@gmail.com>
2023-04-23 22:30:31 -0600

    Tercer commit - Se añadio la funcionalidad al index (Mostrar registro de actividad de los empleados y sueldos) y añadir empleados nuevos a la base de datos. El siguiente paso será implementar la funcionalidad para Registrar Actividad

--- routes.js
+++ routes.js
@@ -41,8 +41,26 @@ dbConnection.connect(err => {
 
 // ====== RUTAS PARA ACCEDER A LAS VISTAS DEL PROYECTO Y OBTENER LOS DATOS DE LA BASE DE DATOS ======
 // Carga la página inicial del proyecto (Index) (Reporte de Actividades)
+// GET /actividad: Retorna una lista con todos los registros de actividades que se han realizado. 
 router.get('/', (req, res) => {
-    res.render('index.ejs', {})
+    const MySQLQuery = "CALL sp_obtenerActividad()";
+
+    dbConnection.query(MySQLQuery, function(err, result) {
+        if (err)
+        {
+            res.send('ERROR: No fue posible realizar la conexión a la BD de Rinku - Luis Eduardo Villela Zavala')
+            throw err
+        }
+        else
+        {
+            console.log("=== LISTA DE ACTIVIDADES ===")
+            let data = JSON.parse(JSON.stringify(result[0])) 
+            console.log(data)
+            res.render('index.ejs', {params: data})
+        }
+    })
+
+    //res.render('index.ejs', {})
     //res.send('Rinku - Sr Fullstack Code Challenge (Coppel) - Luis Eduardo Villela Zavala')
 })
 
@@ -56,6 +74,43 @@ router.get('/nuevo_empleado', (req, res) => {
     res.render('nuevo_emp.ejs', {})
 })
 
+// POST /addNuevoEmpleado: Crea un nuevo empleado, lo almacena en la base de datos y retorna su información.
+router.post('/addNuevoEmpleado', (req, res) => {
+    // Se obtienen los parámetros directamente del body.
+    const {numEmpleado, nombreEmp, apellidoEmp, idRol} = req.body
+
+    // Se declara la ejecución del Procedimiento Almacenado correspondiente
+    const MySQLQuery = "CALL crear_empleado(" + numEmpleado +", '"+ nombreEmp +"', '"+ apellidoEmp +"', "+ idRol + ")";
+    console.log(MySQLQuery)
+
+    // Se inicializa la conexión a la base de datos y se intenta ejecutar el procedimiento almacenado.
+    dbConnection.query(MySQLQuery, function(err, result) {
+        if (err)
+        {
+            console.log("ERROR: ", err)
+            res.redirect("/error")
+        }
+        else
+        {
+            console.log(req.body)
+            res.redirect("/success")
+        }
+
+        //res.status(200).json(req.body)
+    })
+})
+
+// Página para mostrar que el registro se realizó de forma satisfactoria
+router.get('/success', (req, res) => {
+    res.render('success.ejs', {})
+})
+
+// Página de error
+router.get('/error', (req, res) => {
+    res.render('error.ejs', {})
+})
+
+// === NOTA: LOS 3 ENDPOINTS SIGUIENTES FUNCIONAN SOLAMENTE UTILIZANDO POSTMAN!!! ===
 // POST /nuevoEmpleado: Crea un nuevo empleado, lo almacena en la base de datos y retorna su información.
 router.post('/nuevoEmpleado', (req, res) => {
     // Se obtienen los parámetros directamente del body.
